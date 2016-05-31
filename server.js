5810777a8a3714bd9299d7687dcf091034e279ef
malaotou <marrator@163.com>
2016-05-31 12:51:07 +0800

    chagne

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app =express();
 app.set('view engine','jade');
 
 app.get('*',function(req,res){
-	res.send('this  ');
+	res.send('杨广超  ');
 })
 var port=process.env.PORT||3000
 app.listen(port);
