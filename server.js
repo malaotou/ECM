b31df5b4ba3700ba29acc5ac57dacff78af326d0
malaotou <marrator@163.com>
2016-05-31 10:41:29 +0800

    chagne

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.set('view engine','jade');
 app.get('*',function(req,res){
 	res.send('this is the demo fdsfadsafds fds site');
 })
-var port=3000||process.env.PORT
+var port=process.env.PORT||3000
 app.listen(port);
