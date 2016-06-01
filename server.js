1ef31569e2d69349b0bd71d1addef106f1fc5d78
malaotou <marrator@163.com>
2016-06-01 11:54:26 +0800

    init 2

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 var express=require('express');
+var bodyParser = require('body-parser');
 var app =express();
+var stylus=require('stylus');
+var logger =require('express-logger')
 
+
+function compile(str,path){
+	return stylus(str).set('filename',null);
+}
+
+
+
+app.set('views', __dirname);
 app.set('view engine','jade');
+app.use(stylus.middleware({
+	src:__dirname+'/public',
+	compile:compile
+}));
+app.use(express.static(__dirname+'/public'));
+app.use(logger({path: "/path/to/logfile.txt"}));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.locals.viewdir=__dirname+'/public/views'
 
 app.get('*',function(req,res){
-	res.send('杨广超  ');
+	
+	res.render('public/index');
 })
 var port=process.env.PORT||3000
 app.listen(port);
+console.log('Server start on port '+port)
